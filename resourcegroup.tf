18b754d8e88bf6b1ee74c02b726a38dfeea6a0a2
Umesh <166121140+umeshkummari@users.noreply.github.com>
2025-06-09 03:59:40 +0530

    Update resourcegroup.tf
    added comments

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,18 +1,25 @@
+#Terraform block
+
 terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">=2.0.0"
+      version = ">=2.0.0" #provider version ie (azure rm version)
     }
   }
-  required_version = ">=1.0.0"
+  required_version = ">=1.0.0" #terraform version
 }
+
+
+#provider block
 
 provider "azurerm" {
   resource_provider_registrations = "none"
   features {}
   subscription_id = "f5380571-80ae-4c56-b0e5-9d1c2ddfb488"
 }
+
+#resource block
 
 resource "azurerm_resource_group" "Firstresource" {
   location = "East Us"
